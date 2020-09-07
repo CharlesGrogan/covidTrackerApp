5870b9a9d54a687ee355c612ef93f61f578566b6
CGROGAN <grogan.charlie@gmail.com>
2020-09-07 14:23:37 -0500

    card creation

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,12 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-const fetchData = async () => {
+export const fetchData = async () => {
+  try {
+    const { data: { confirmed, recovered, deaths, lastUpdated} } = await axios.get(url);
+    
+    return { confirmed, recovered, deaths, lastUpdated};
+  } catch (error) {
 
+  }
 }
